26980eabe2a030c921832b8eb0f5b799b1e13072
= <=>
2025-12-03 16:51:14 +0700

    Menambahkan file belajar.sql

---
+++ belajar.sql
@@ -0,0 +1,26 @@
+select * from barang
+
+create table products
+(
+    id varchar(10) not null,
+    name varchar(100) not null,
+    description text,
+    price  int not null,
+    quantity    int not null default 0,
+    created_at timestamp not null default current_timestamp
+)
+
+insert into products(id,name,price,quantity)
+values ('P0001','Mie Ayam Original',15000,100)
+
+insert into products(id,name,description,price,quantity)
+values('P0002','Mie Ayam Bakso Tahu', 'Mie Ayam Original + Bakso Tahu',20000,100);
+
+insert into products(id,name,price,quantity)
+values ('P0003','Mie Ayam Ceker', 20000, 100),
+       ('P0004','Mie Ayam Spesial', 25000,100),
+       ('P0005','Mie Ayam Yamin', 15000,100);
+
+select * from products
+
+select id,name, price, quantity from products
