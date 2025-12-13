27fbfc69042155a90737a18ad52f61a46262a799
= <=>
2025-12-13 22:55:54 +0700

    Menambahkan materi TABLE RELATIONSHIP

---
+++ table-relationship.sql
@@ -0,0 +1,118 @@
+create table wishlist
+(
+    id serial not null primary key,
+    id_product varchar(10) not null,
+    description text,
+    constraint fk_wishlist_products foreign key (id_product) references products (id)
+);
+
+select * from products;
+
+insert into wishlist(id_product,description)
+values('P0001', 'Mie Ayam Kesukaan'),
+      ('P0002', 'Mie Ayam Kesukaan'),
+      ('P0005', 'Mie Ayam Kesukaan');
+
+select * from wishlist;
+
+alter table wishlist
+drop constraint fk_wishlist_products;
+
+alter table wishlist
+add constraint fk_wishlist_products foreign key(id_product) references products(id)
+on delete cascade on update cascade;
+
+insert into products(id,name,price, quantity, category)
+values('XXX','XXX',10000,100,'Minuman')
+
+select * from products;
+select * from wishlist;
+
+insert into wishlist(id_product,description)
+values ('XXX','Contoh');
+
+delete from products
+where id = 'XXX';
+
+select * from wishlist
+join products on products.id = wishlist.id_product;
+
+select products.id , products.name, wishlist.description
+from wishlist
+join products on products.id = wishlist.id_product;
+
+alter table wishlist
+add column id_customer int;
+
+alter table wishlist
+add constraint fk_wishlist_customer foreign key (id_customer) references customer(id);
+
+update wishlist
+set id_customer = 1
+where id in (2,3);
+
+update wishlist
+set id_customer = 4
+where id = 4
+
+select * from customer;
+
+select customer.email, products.id , products.name, wishlist.description from wishlist
+join products on products.id = wishlist.id_product
+join customer on customer.id = wishlist.id_customer;
+
+create table wallet
+(
+    id serial not null primary key ,
+    id_customer int not null,
+    balance int not null default 0,
+    constraint wallet_customer_unique unique (id_customer),
+    constraint fk_wallet_customer foreign key (id_customer) references customer (id)
+);
+
+select * from customer;
+
+insert into wallet(id_customer,balance)
+values (1,1000000),
+       (3,2000000),
+       (4,3000000),
+       (5,4000000);
+
+select * from wallet
+
+select * from customer
+join wallet on wallet.id_customer = customer.id;
+
+create table categories
+(
+    id varchar(10) not null primary key,
+    name varchar(100) not null
+);
+
+insert into categories(id, name)
+values('C0001','Makanan'),
+      ('C0002','Minuman');
+
+drop table category;
+select * from categories;
+alter table products
+drop column category;
+
+alter table products
+add column id_category varchar(10);
+
+alter table products
+add constraint fk_product_category foreign key (id_category) references categories(id);
+
+select * from products;
+
+update products
+set id_category = 'C0001'
+where id in ('P0001','P0002','P0005','P0003','P0004');
+
+update products
+set id_category = 'C0002'
+where id in ('P0007','P0008','P0006');
+
+select * from products
+join categories on products.id_category = categories.id;
